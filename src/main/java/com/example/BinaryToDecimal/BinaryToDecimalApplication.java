21ef6debe7ae6c58c2f179716228200691f721e4
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2022-02-18 08:46:16 +0100

    First

---
+++ src/main/java/com/example/BinaryToDecimal/BinaryToDecimalApplication.java
@@ -0,0 +1,13 @@
+package com.example.BinaryToDecimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryToDecimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryToDecimalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/BinaryToDecimal/BinaryToDecimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BinaryToDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryToDecimalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
